ac2969a6f4e47fbc4c5e1cc8b0efbcae5b167b5c
Rijan Bhattarai <rijanbhattarai2006@gmail.com>
2025-11-11 10:57:04 +0545

    Update pr_01.java to add user input and sum calculation
    Replaced Random import with Math, added prompts for user input of two numbers, and output their sum. Also added BlueJ context file and compiled class file.

--- College/pr_01.java
+++ College/pr_01.java
@@ -1,8 +1,19 @@
 import java.util.Scanner;
-import java.util.Random;
+import java.lang.Math;
+
 
 public class pr_01 {
     public static void main(String[] args) {
+        Scanner src = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int x = src.nextInt();
         
+        System.out.print("Enter 2nd number: ");
+        int y = src.nextInt();
+        
+        System.out.println("Sum = " + (x + y));
+        
+        src.close();
     }
 }
